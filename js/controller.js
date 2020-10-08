defd15b822e638de2c65e60a82e87c0e6d2b5987
yang-520199 <951333953@qq.com>
2020-10-09 00:10:54 +0800

    实习界面

---
+++ js/controller.js
@@ -0,0 +1,44 @@
+function f() {
+    var bar = document.getElementById("bar");
+    if (bar.style.display == "none") {
+        bar.style.display = "";
+    } else {
+        bar.style.display = "none"
+    }
+
+}
+
+function s() {
+    var bar = document.getElementById("showdata");
+    if (bar.style.display == "none") {
+        bar.style.display = "";
+    } else {
+        bar.style.display = "none";
+    }
+
+}
+
+function pic() {
+    var bar = document.getElementById("bar");
+    if (bar.style.display == "none") {
+        bar.style.display = "";
+    } else {
+        bar.style.display = "none";
+    }
+    var bar = document.getElementById("show");
+    if (bar.style.display == "none") {
+        bar.style.display = "";
+    } else {
+        bar.style.display = "none";
+    }
+
+}
+
+function s1() {
+    document.getElementById("showdata").style.display = "none";
+}
+
+function pic1() {
+    document.getElementById("bar").style.display = "none";
+    document.getElementById("show").style.display = "none";
+}
